2cc876a72c7992f80d4cfe5d36b0d0f985b6ddbe
rozalii <ijaqhaha@gmail.com>
2019-02-21 01:46:58 +0800

    add some code

--- utils/transforms.py
+++ utils/transforms.py
@@ -2,6 +2,8 @@ import PIL
 import PIL.Image
 import torch.nn.functional as F
 import torchvision.transforms as transforms
+import cv2
+import torch
 
 def idx_to_class(class_to_idx):
     idx2class = {}
@@ -17,6 +19,35 @@ def get_hand_number(classes, class_to_idx):
     for key in nclass:
         name.append(idx2class[key])
     return nclass, name
+    
+class ResizeCV(object):
+    def __init__(self, size):
+        self.size = size
+    
+    def __call__(self, image):
+        rs_img = cv2.resize(image, (self.size,self.size))
+        return rs_img
+        
+        
+class NumpyToTensor(object):
+    def __init__(self):
+        pass
+    def __call__(self, image):
+        return torch.from_numpy(image)
+    
+        
+def preprocess_npimage(img):
+    
+    mean_val, std_val = [0.485, 0.456, 0.406], [0.229, 0.224, 0.225]
+    do_transforms =  transforms.Compose([
+        ResizeCV(size=224),
+#         transforms.CenterCrop(224),
+        NumpyToTensor(),
+        transforms.Normalize(mean_val,std_val)
+    ])
+    im_tfmt = do_transforms(img)
+    im_add_batch = im_tfmt.view(1, im_tfmt.shape[0], im_tfmt.shape[1], im_tfmt.shape[2])
+    return im_add_batch
     
 
 def process_image(image):
